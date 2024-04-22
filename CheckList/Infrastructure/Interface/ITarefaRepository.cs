d4ec84322be20e28c1acad22785a942c69e6f422
Paulo Couto <paulo@financas360.com.br>
2024-04-22 09:14:07 -0300

    Corrigido bug de alteração de status

--- CheckList/Infrastructure/Interface/ITarefaRepository.cs
+++ CheckList/Infrastructure/Interface/ITarefaRepository.cs
@@ -8,6 +8,6 @@ namespace CheckList.Infrastructure.Interface
         TarefaModel? ObterPorId(int id);
         void Adicionar(TarefaModel tarefa);
         void Deletar(int id);
-        void Atualizar(int id);
+        void Atualizar(TarefaModel tarefa);
     }
 }
